ce3674e983c0336ee8439e0cbd226df24639ced7
Rodrigo Souza <ro_web1@hotmail.com>
2016-09-30 16:42:05 -0300

    setting up workflow

---
+++ Gulpfile.babel.js
@@ -0,0 +1,28 @@
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import cssnano from 'cssnano';
+import postcss from 'gulp-postcss';
+import poststylus from 'poststylus';
+import autoprefixer from 'autoprefixer';
+
+
+gulp.task('styles', ()=> {
+	gulp.src('./src/stylus/main.styl')
+	.pipe(plumber())
+  .pipe(stylus({
+    use:[
+      poststylus([ autoprefixer({ browsers: ["last 3 version", "> 1%", "ie 8"] })]),
+    ]
+  }))
+  .pipe(postcss([
+  	cssnano()
+  ]))
+  .pipe(gulp.dest('./build/css/'))
+});
+
+gulp.task('watch', ()=> {
+  gulp.watch('./src/stylus/*.styl', ['styles']);
+});
+
+
+gulp.task('default', ['watch']);
